edd8a7f51aec472a98125407aaf071271ccdc83d
SzmidtAn <a.szmidt95@gmail.com>
2020-09-15 10:43:27 +0200

    test new branch

--- app/src/main/java/com/example/lektion2/Element.kt
+++ app/src/main/java/com/example/lektion2/Element.kt
@@ -6,18 +6,7 @@ import android.view.MotionEvent
 class Element (var imageString: Int, var elementId:Int, var ifFind:Boolean = false) {
 
 
-     fun onTouchEvent(event: MotionEvent?): Boolean {
-        when( event?.action) {
-            MotionEvent.ACTION_DOWN -> {
-                imageString = R.drawable.ic_launcher_background
-            }
-            MotionEvent.ACTION_UP -> {
-                imageString=R.drawable.ic_launcher_background
 
-            }
-        }
-        return true
-    }
 
 
 }
